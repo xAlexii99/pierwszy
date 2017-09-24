1b6d1046f42014718e21db826c2d7ffc7f067b23
unknown <k.aleksander@vp.pl>
2017-09-24 08:53:04 +0200

    zad7

--- pierwsza/app/src/main/java/pl/aleksander/pierwsza/MainActivity.java
+++ pierwsza/app/src/main/java/pl/aleksander/pierwsza/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.zad6);
+        setContentView(R.layout.zad7);
     }
 }
